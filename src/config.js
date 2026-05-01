5c2902c29252826e5fcf1b7269f7c958e39ebcf5
Moik <80180228+MightyDash@users.noreply.github.com>
2026-05-01 11:52:25 +0200

    Update SHEET_API_URL to point to a Google Sheet

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // Voorbeeld:
 // export const SHEET_API_URL = "https://script.google.com/macros/s/AKfycb.../exec";
 
-export const SHEET_API_URL = "";
+export const SHEET_API_URL = "https://docs.google.com/spreadsheets/d/1MPU6qaRq-JNPhpqptGYLRazDCZHmu2Ur23ri0Y4bzzA/edit?usp=sharing";
